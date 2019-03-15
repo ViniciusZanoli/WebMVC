ba2d91c4c8086c137006d09c965c4cdf2ea01d59
Vinicius Zanoli <viniciusdasilva.92@hotmail.com>
2019-03-15 00:58:09 -0300

    Criado Serviço SellerService / Views-Seller

---
+++ WebMVC/Services/SellerService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebMVC.Models;
+
+namespace WebMVC.Services
+{
+    public class SellerService
+    {
+        // readonly SIGNIFICA QUE NÃO PODE SER ALTERADO
+        private readonly WebMVCContext _context;
+
+        public SellerService(WebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
--- WebMVC/Startup.cs
+++ WebMVC/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using WebMVC.Models;
 using WebMVC.Data;
+using WebMVC.Services;
 
 namespace WebMVC
 {
@@ -42,6 +43,7 @@ namespace WebMVC
                         builder.MigrationsAssembly("WebMVC")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
